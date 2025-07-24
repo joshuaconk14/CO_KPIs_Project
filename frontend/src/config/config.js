6dc92074cc4112afc34e84f7844e0b820031a5d7
Joshua Conklin <joshuaconk14>
2025-07-23 18:01:25 -0700

    Redeploy backend

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -4,8 +4,8 @@ const config = {
     wsUrl: 'ws://localhost:8080/ws/kpi'
   },
   production: {
-    apiUrl: process.env.REACT_APP_API_URL || 'https://co-kpi-backend.herokuapp.com',
-    wsUrl: process.env.REACT_APP_WS_URL || 'https://co-kpi-backend.herokuapp.com/ws/kpi'
+    apiUrl: process.env.REACT_APP_API_URL || 'https://co-kpi-backend-063baf45995f.herokuapp.com',
+    wsUrl: process.env.REACT_APP_WS_URL || 'https://co-kpi-backend-063baf45995f.herokuapp.com/ws/kpi'
   }
 };
 
